896fe4a1300bc48a243ef56730fb618c7cde569a
Joseph Venedict Tillo <119979348+Sedictt@users.noreply.github.com>
2025-12-06 10:31:41 +0800

    feat: Add booking system with email notifications, new site pages, and updated development scripts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import Gallery from './pages/Gallery';
+import EmailTest from './pages/EmailTest';
+
+import { BookingProvider } from './context/BookingContext';
+import BookingModal from './components/BookingModal';
 
 function App() {
     return (
-        <Router>
-            <div className="app-container">
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/services" element={<Services />} />
-                    <Route path="/gallery" element={<Gallery />} />
-                </Routes>
-                <Footer />
-            </div>
-        </Router>
+        <BookingProvider>
+            <Router>
+                <div className="app-container">
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/services" element={<Services />} />
+                        <Route path="/gallery" element={<Gallery />} />
+                        <Route path="/email-test" element={<EmailTest />} />
+                    </Routes>
+                    <Footer />
+                    <BookingModal />
+                </div>
+            </Router>
+        </BookingProvider>
     );
 }
 

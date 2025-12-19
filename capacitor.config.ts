b9e99e6dfb017b756bf85d49214a67b0ca69eaf5
Joseph Venedict Tillo <119979348+Sedictt@users.noreply.github.com>
2025-12-19 17:42:32 +0800

    more responsiveness tweak

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.itsourstudio.admin',
-  appName: 'ItsOurStudioAdmin',
+  appName: 'IoS Admin',
   webDir: 'dist'
 };
 
